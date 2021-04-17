955f9415076781d213ff8b1ce3893e38c1a55e9d
Maciej Paszkowski <maciej.paszkowski@gmail.com>
2021-04-17 16:40:04 +0200

    addind Favorites to Project and synchronizing with redux

--- src/components/Currency/Currency.js
+++ src/components/Currency/Currency.js
@@ -1,13 +1,28 @@
 import React from "react";
 
-const Currency = ({ data }) => {
+import PropTypes from "prop-types";
+import MyButton from "../MyButton";
+
+const Currency = ({ data, onClick, icon }) => {
   return (
     <tr className="table__tr">
       <td className="table__td">{data.code}</td>
       <td className="table__td">{data.currency}</td>
-      <td className="table__td">{data.mid}</td>
+      <td className="table__td table__td--flex">
+        {data.mid}
+        {"    "}
+        <MyButton icon={icon} onClick={() => onClick(data.code)} />
+      </td>
     </tr>
   );
+};
+
+Currency.propTypes = {
+  data: PropTypes.shape({
+    code: PropTypes.string.isRequired,
+    currency: PropTypes.string.isRequired,
+    mid: PropTypes.number.isRequired,
+  }),
 };
 
 export default Currency;
